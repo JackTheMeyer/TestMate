bcfcc5d61da0e206041bc732983decb3b1fd0c11
Jack <jackmeyer@live.com.au>
2018-11-09 13:07:19 +1100

    added ClassLibrary

---
+++ MateLibrary/Account.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateLibrary
+{
+    public class Account
+    {
+        private string _bsb;
+        private string _accountNumber;
+        private double _balance;
+        private string _name;
+
+        public Account(string account)
+        {
+            _accountNumber = account;
+            
+        }
+
+        public string BSB
+        {
+            get
+            {
+                return _bsb;
+            }
+        }
+
+        public string AccountNumber
+        {
+            get
+            {
+                return _accountNumber;
+            }
+        }
+
+        public double Balance
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+    }
+}
---
+++ MateLibrary/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateLibrary
+{
+    public class User
+    {
+        private string _username;
+        private string _password;
+        List<Account> _accounts = new List<Account>();
+        public User(string username, string password)
+        {
+            _username = username;
+            _password = password;
+        }
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+        }
+
+        public List<Account> AccountList
+        {
+            get
+            {
+                return _accounts;
+            }
+        }
+
+        public void OpenAccount(string AccountNumber, double balance)
+        {
+
+        }
+
+        public void SendTransfer(Account sender, Account reviever, double balance)
+        {
+
+        }
+
+    }
+}
