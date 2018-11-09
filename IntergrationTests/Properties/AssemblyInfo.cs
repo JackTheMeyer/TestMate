7c793ff9e01c397dabba722d4a7757cdef97e61e
Jack <jackmeyer@live.com.au>
2018-11-09 12:23:25 +1100

    RenamedFiles

---
+++ IntergrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IntergrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IntergrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8ab9d65b-14f3-4089-ab4a-7bc68d352bd4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ IntergrationTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankMate;
+
+namespace BankMate
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestTransfer()
+        {
+            //Recieve Transfer Request
+
+            // BankingSystem.Transfer(Sender, Reciever, Ammount
+            
+            //Account Located
+            // BankingSystem.Locate(Reciever, Balance)
+            Assert.AreEqual(Program.CreateMessage(), "Hello World");
+        }
+    }
+}
