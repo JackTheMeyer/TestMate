3d07ef0ffa4604bbc025e5602df6e81d75af9535
Jack <jackmeyer@live.com.au>
2018-11-09 13:34:17 +1100

    FixedReferencing

---
+++ BankApp/BankingSystem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateLibrary
+{
+    public class BankingSystem
+    {
+        private List<User> _userList = new List<User>();
+        public BankingSystem()
+        {
+            
+        }
+
+        public List<User> Users
+        {
+            get
+            {
+                return _userList;
+            }
+        }
+ 
+        public void TransferComplete()
+        {
+
+        }
+
+        public void Transfer(Account sender, Account reciever, double amount)
+        {
+
+        }
+
+        public void Locate(Account reciever)
+        {
+
+        }
+       
+    }
+}
